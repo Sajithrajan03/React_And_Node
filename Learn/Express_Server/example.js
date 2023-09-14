f7a56540f217e28af829019c9d290f9db82343fa
Sajithrajan03 <saisajith03@gmail.com>
2023-09-14 21:24:06 +0530

    cs

--- Learn/Express_Server/example.js
+++
@@ -1,7 +0,0 @@
-import http from 'http';
-function rqlisten(req,res){
-    console.log(req);
-}
-const server = http.createServer(rqlisten);
-
-server.listen(3000);
--- Learn/Express_Server/sample.py
+++
@@ -1,6 +0,0 @@
-for _ in range(2):
-    a = int(input())
-    s = input()
-    d = int(s[len(s)-3:])
-    print(int(s)+1,s,d)
-    
