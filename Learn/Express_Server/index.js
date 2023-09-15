e2e557851807060e62aa481bfbd19483e0ca87e4
Sajithrajan03 <saisajith03@gmail.com>
2023-09-15 15:29:45 +0530

    cs

--- Learn/Express_Server/index.js
+++ Learn/Express_Server/index.js
@@ -2,30 +2,34 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { log } from "console";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-var bandName = "";
-
+var password = "";
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function bandNameGenerator(req, res, next) {
-  console.log(req.body);
-  bandName =  req.body["street"] + req.body["pet"];
+function check(req, res, next) {
+  console.log(req.body['Password']);
+  password= req.body['Password']
   next();
 }
 
-app.use(bandNameGenerator);
+app.use(check);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
-  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
-});
+  
+  password =  req.body["Password"];
+  if (password ==="ppp"){
+  res.sendFile(__dirname +"/public/index1.html");
+
+}});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
